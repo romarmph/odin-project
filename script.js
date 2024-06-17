82b38cc0713142848bfc7b432fe421e89f5ef840
Romar Macaraeg <romarmph@gmail.com>
2024-06-18 01:31:42 +0800

    Add game ui
    Add some basic game logic including
    game and display controllers

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const game = function() {
 	let board = [...Array(9)]
-	let currentPlayer = 'x'; // initial player
+	let currentPlayer = 'x';
 
 	function setBoard(newBoardState) {
 		if (!Array.isArray(newBoardState)) {
@@ -46,36 +46,95 @@ const game = function() {
 	}
 }()
 
-const gameController = function() {
-	function resetGameBoard() {
-		return [...Array(9)];
-	}
-
-	return {
-		resetGameBoard,
-	}
-}()
 
 const displayController = function() {
+
+	/**
+		* @param {object} game The game object
+		* @returns {array} cells An array of cell nodes for the game board
+	 */
+	function createBoardCells(game) {
+		if (!game) {
+			throw new Error('Game is undefined');
+		}
+		const board = game.getBoard();
+		const cells = board.map(() => {
+			const div = document.createElement('div');
+			div.classList.add('cell');
+			document.querySelector('#board').append(div)
+			return div;
+		})
+
+		return cells;
+	}
+
 	function renderBoard(game) {
 		if (!game) {
 			throw new Error('Game is undefined');
 		}
 
-		const currentGameBoard = game.getBoard();
-		console.log(`
-				Current Game
-				[${currentGameBoard[0]},${currentGameBoard[1]},${currentGameBoard[2]}],
-				[${currentGameBoard[3]},${currentGameBoard[4]},${currentGameBoard[5]}],
-				[${currentGameBoard[6]},${currentGameBoard[7]},${currentGameBoard[8]}],
-			`)
+		let cells;
+		if (!cells) {
+			cells = createBoardCells(game);
+		}
+
+		cells.forEach((cell, index) => {
+			const gameBoard = game.getBoard();
+			cell.classList.add(gameBoard[index] ?? 'empty');
+		})
 	}
 
-	function renderResult(game) {
+	function renderResult(result) {
+		const resultContainer = document.createElement('div');
+		resultContainer.classList.add('result');
+		resultContainer.innerHTML = '<h2>Game Over</h2>';
 
+		if (result === 'draw') {
+			resultContainer.innerHTML = `
+		<h2>Game Over</h2>
+		<p>Draw!</p>
+			`;
+		} else if (result === 'x') {
+			resultContainer.innerHTML = `
+		<p>Winner!</p>
+		<img src="assets/x.png" alt="x-player-icon">
+			`
+		} else if (result === 'o') {
+			resultContainer.innerHTML = `
+		<p>Winner!</p>
+		<img src="assets/o.png" alt="o-player-icon">
+			`
+		} else {
+			throw new Error(`Invalud result '${result}'`);
+		}
+
+		const board = document.querySelector('.board__container');
+		document.querySelector('.container').insertBefore(resultContainer, board)
 	}
 
 	return {
 		renderBoard,
+		renderResult,
+		createBoardCells,
 	}
 }()
+
+const gameController = function() {
+	function startGame(game, displayController) {
+		const cells = displayController.createBoardCells(game);
+
+		cells.forEach((cell, index) => {
+			cell.addEventListener('click', () => {
+				console.log(game.getCurrentPlayer(), index);
+			})
+		})
+	}
+
+	return {
+		startGame,
+	}
+}()
+
+window.addEventListener('load', () => {
+	gameController.startGame(game, displayController);
+})
